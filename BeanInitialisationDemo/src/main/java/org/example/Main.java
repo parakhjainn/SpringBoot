79996bba7dab6b5566ec101a93abee98e0e70112
Parakh Jain <parakhjainwork@gmail.com>
2026-06-28 15:47:42 +0530

    Bean Initialisation Demo Setup

---
+++ BeanInitialisationDemo/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    static void main() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    }
+}
