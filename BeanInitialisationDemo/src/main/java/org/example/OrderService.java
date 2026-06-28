7e9ec843aab2688dd34bd96132c4828187b4101b
Parakh Jain <parakhjainwork@gmail.com>
2026-06-28 15:51:42 +0530

    Bean Initialisation - Default is Eager

---
+++ BeanInitialisationDemo/src/main/java/org/example/OrderService.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderService {
+
+    public OrderService() {
+        System.out.println("OrderService created !");
+    }
+}
---
+++ BeanInitialisationDemo/src/main/java/org/example/PaymentService.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentService {
+
+    public PaymentService(){
+        System.out.println("PaymentService Created !");
+    }
+}
