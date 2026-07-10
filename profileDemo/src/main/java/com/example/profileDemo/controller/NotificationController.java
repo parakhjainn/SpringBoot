0436a5c76a834968c84909dee31f679cdcccf6d4
Parakh Jain <parakhjainwork@gmail.com>
2026-07-10 23:11:11 +0530

    Notification Service without profile

---
+++ profileDemo/src/main/java/com/example/profileDemo/controller/NotificationController.java
@@ -0,0 +1,26 @@
+package com.example.profileDemo.controller;
+
+import com.example.profileDemo.service.NotificationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/notification")
+public class NotificationController {
+
+    private NotificationService notificationService;
+
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> sendNotification() {
+        String notification = notificationService.send();
+
+        return ResponseEntity.ok(notification);
+
+    }
+}
---
+++ profileDemo/src/main/java/com/example/profileDemo/service/NotificationService.java
@@ -0,0 +1,13 @@
+package com.example.profileDemo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+
+    public String send() {
+        // email // sms
+
+        return "Notification !";
+    }
+}
